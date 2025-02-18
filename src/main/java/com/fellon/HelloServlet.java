0bb6106a55420639680a6f11dfc2852000da89c2
fellon-dnb <fellon@ancevt.com>
2025-02-19 00:18:51 +0300

    я заебался

--- src/main/java/com/fellon/HelloServlet.java
+++ src/main/java/com/fellon/HelloServlet.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    private static final String VERSION = "1.0";
+    private static final String VERSION = "2.0";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
