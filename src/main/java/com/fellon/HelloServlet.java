d02fa438753358a63723fd02a78e1bb2f0fbabfa
fellon-dnb <fellon@ancevt.com>
2025-02-18 22:39:40 +0300

    Normalize line endings

---
+++ src/main/java/com/fellon/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.fellon;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final String VERSION = "1.0";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<p>Version: " + VERSION + "</p>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to My Web Application</h1>
+    <p><a href="hello">Click here to see the servlet response</a></p>
+</body>
+</html>
